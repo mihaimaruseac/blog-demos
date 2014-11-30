f86f6a647e8ca1422b90a2100311925a0be9e502
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-11-30 10:20:00 -0500

    (Re)Add acidic state info.

--- acid-state/Main.hs
+++ acid-state/Main.hs
@@ -31,9 +31,12 @@ instance Indexable Details where
 data Test = Test
   { nextID :: Int
   , details :: IxSet Details
-  }
+  } deriving (Typeable)
 
 $(deriveSafeCopy 0 'base ''Test)
+
+initialState :: Test
+initialState = Test 0 empty
 
 {-
 cleanTest :: Update Test0()
@@ -54,12 +57,14 @@ insertTest x = modify (Test0 . (x:) . elems)
 $(makeAcidic ''Test0 ['queryTest, 'cleanTest, 'insertTest, 'sumTest, 'sizeTest])
 -}
 
+$(makeAcidic ''Test [])
+
 main :: IO ()
 main = do
   args' <- cmdArgs testArgs
   case args' of
     Help -> showHelp
-    --_ -> mainDB args'
+    _ -> mainDB args'
 
 showHelp :: IO ()
 showHelp = print $ CA.helpText [] CA.HelpFormatAll $ cmdArgsMode testArgs
@@ -73,20 +78,20 @@ timeIt header action = do
   print $ diffUTCTime endTime stTime
   return a
 
-{-
 mainDB :: TestArgs -> IO ()
 mainDB arg = do
-  st <- timeIt "Open state: " $ openLocalState $ Test0 []
+  st <- timeIt "Open state: " $ openLocalState initialState
   _ <- case arg of
-    List -> timeIt "List time: " $ dump st
-    Clean -> timeIt "Clean time: " $ clean st
-    GC -> timeIt "GC time: " $ createCheckpoint st >> createArchive st
-    Insert x -> timeIt "Insertion time: " $ insert st x
-    Sum -> timeIt "Sum computation: " $ sumDB st
-    Size -> timeIt "Size computation: " $ size st
+    -- List -> timeIt "List time: " $ dump st
+    -- Clean -> timeIt "Clean time: " $ clean st
+    -- GC -> timeIt "GC time: " $ createCheckpoint st >> createArchive st
+    -- Insert x -> timeIt "Insertion time: " $ insert st x
+    -- Sum -> timeIt "Sum computation: " $ sumDB st
+    -- Size -> timeIt "Size computation: " $ size st
     _ -> error "Should be handled before this point"
   timeIt "Close state: " $ closeAcidState st
 
+{-
 dump :: AcidState (EventState QueryTest) -> IO ()
 dump st = query st QueryTest >>= print
 
@@ -115,12 +120,12 @@ data TestArgs
 
 testArgs :: TestArgs
 testArgs = modes
-  [ Clean &= help "Clean DB (reset number list)"
+  [ Clean &= help "Clean DB"
   , GC &= help "Garbage collect, reduce size of files to increase speed"
-  , Insert { number = def &= argPos 0 } &= help "Insert new number"
-  , List &= help "List numbers"
+  , Insert { number = def &= argPos 0 } &= help "Insert new entry"
+  , List &= help "List entries"
   , Size &= help "Return size of DB"
-  , Sum &= help "Sum numbers"
+  , Sum &= help "Sum numbers from entries"
   , Help &= help "Show this help message" &= auto
   ]
   &= help "Test acid-state library"
