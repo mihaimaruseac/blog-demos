e4b71f7b86e6aec586b083dd086c07207b717aaa
Mihai Maruseac <mihai.maruseac@gmail.com>
2019-04-26 22:46:48 -0700

    Something is wrong, needs debug

--- azpcs/1.c
+++ azpcs/1.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
-#define N 5
+#define N 2 //5
 #define N2 (N * N)
+#define MIN_BOUND 3800
+#define POPSZ 4 //100
+#define FAMSZ 3
 
-const char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234";
-unsigned int d[N2][N2];
+#define OO 99999999999L
 
-const int grid1[] = {20, 8, 10, 19, 2, 17, 16, 1, 12, 6, 0, 14, 18, 15, 21, 23, 3, 4, 13, 7, 9, 5, 24, 22, 11};
-const int grid2[] = {12, 15, 24, 2, 18, 0, 20, 7, 17, 5, 22, 10, 8, 19, 9, 3, 23, 11, 21, 16, 1, 13, 6, 4, 14};
+static const char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234";
+static unsigned long d[N2][N2];
+
+static int pop1[POPSZ][N2];
+static int pop2[POPSZ][N2];
+static unsigned long scores[POPSZ];
+static int epoch = 0;
+static unsigned long best_score = OO;
+static int best[N2];
 
 static inline void print_one(int x)
 {
@@ -33,39 +43,103 @@ start:
 	printf(")\n");
 }
 
-static inline unsigned int dist(int i, int j)
+static inline unsigned long dist(int i, int j)
 {
-	unsigned int dx = abs((i % N) - (j % N));
-	unsigned int dy = abs((i / N) - (j / N));
+	unsigned long dx = abs((i % N) - (j % N));
+	unsigned long dy = abs((i / N) - (j / N));
 	if (dx > N - dx) dx = N - dx;
 	if (dy > N - dy) dy = N - dy;
 	return dx * dx + dy * dy;
 }
 
-static inline void compute_initial()
+static inline void compute_initial_distances()
 {
 	for (int i = 0; i < N2; i++)
 		for (int j = 0; j < N2; j++)
 			d[i][j] = dist(i, j);
 }
 
-static inline unsigned int compute_score(const int x[])
+static inline unsigned long compute_score(const int x[])
 {
-	unsigned int s = 0;
+	unsigned long s = 0;
 	for (int i = 0; i < N2; i++)
 		for (int j = i + 1; j < N2; j++)
 			s += d[i][j] * dist(x[i], x[j]);
-	return s;
+	return s - MIN_BOUND;
+}
+
+static inline void init_rng(void)
+{
+	struct timeval now;
+	gettimeofday(&now, NULL);
+	srand48(42);//now.tv_usec + now.tv_sec);
+}
+
+static inline void random_init(int x[])
+{
+	/* first write array in order */
+	for (int i = 0; i < N2; i++) x[i] = i;
+
+	/* now do a Fisher-Yates shuffle of x */
+	for (int i = N2 - 1; i > 0; i--) {
+		int j = drand48() * i;
+		int t = x[i];
+		x[i] = x[j];
+		x[j] = t;
+	}
+	
+	// debug; print array fast
+	for (int i = 0; i < N2; i++) printf("%d ", x[i]);
+	printf("\n");
+}
+
+static inline void initialize_population(void)
+{
+	for (int i = 0; i < POPSZ; i++)
+		random_init(pop1[i]);
+
+	// debug print
+	for (int i = 0; i < POPSZ; i++) {
+		printf("Pop %d: ", i);
+		for (int j = 0; j < N2; j++) {
+			printf("%d ", pop1[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+static inline void compute_scores()
+{
+	if (epoch % 2 == 0) {
+		for (int i = 0; i < POPSZ; i++)
+			scores[i] = compute_score(pop1[i]);
+	} else {
+		for (int i = 0; i < POPSZ; i++)
+			scores[i] = compute_score(pop2[i]);
+	}
+
+	unsigned long best_now = scores[0];
+	int best_ix = 0;
+	for (int i  = 1; i < POPSZ; i++)
+		if (best_now > scores[i]) {
+			best_now = scores[i];
+			best_ix = i;
+		}
+
+	// debug
+	printf("Scores: ");
+	for (int i = 0; i < POPSZ; i++)
+		printf("%lu ", scores[i]);
+	printf("\nBest: %lu (%d)\n", best_now, best_ix);
 }
 
 int main()
 {
-	compute_initial();
-	printf("Grid 1: %u\n", compute_score(grid1));
-	printf("Grid 2: %u\n", compute_score(grid2));
-	int init[N2];
-	for (int i = 0; i < N2; i++) init[i] = i;
-	printf("No change: %u\n", compute_score(init));
+	init_rng();
+	compute_initial_distances();
+	initialize_population();
+	compute_scores();
+
 
 	return 0;
 }
